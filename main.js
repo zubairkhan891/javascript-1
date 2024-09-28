0b4b3150fb6ae9d5cf414c108532502db1ee628f
zubairkhan891 <zubairkhan891891@gmail.com>
2024-09-29 00:55:49 +0500

    Loop and forLoop

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
-const accountId = 144553
-let accountEmail = "zub@google.com"
-var accountPassword = "123456"
-accountCity = "Jaipur"
+// const accountId = 144553
+// let accountEmail = "zub@google.com"
+// var accountPassword = "123456"
+// accountCity = "Jaipur"
 
  
- //accountId = 2
+//  //accountId = 2
 
- accountEmail =  "zub@google.com"
- accountPassword = "123456"
- accountCity = "Jaipur"
- console.log(accountId);
+//  accountEmail =  "zub@google.com"
+//  accountPassword = "123456"
+//  accountCity = "Jaipur"
+//  console.log(accountId);
 
- console.table([accountId, accountEmail, accountPassword, accountCity]);
+//  console.table([accountId, accountEmail, accountPassword, accountCity]);
  
-function Array() {
-    const arry = [1,2,3]
-     console.log(arry)
-}
+// function Array() {
+//     const arry = [1,2,3]
+//      console.log(arry)
+// }
+
+
+// var num1 = 30
+// var num2 = 40
+
+// var addition = num1 + num2
+// var subtr = num1 - num2
+
+// // alert(addition)
+
+// console.log(addition)
+// console.log(subtr)
+
+ //Loop
+
+//  for (var i = 1 ; i <= 7; i++) {
+//     console.log(i + 'chakkar')
+//  }
+
+//  for (var i = 1; i <= 20; i++ ) {
+//     console.log(' 2 x ' + i + ' = ' + 2 * i)
+//  }
+
+ tableNumber = prompt('Table number')
+  for (var i = 1; i <= 10; i++) {
+    document.write(tableNumber + 'x' + i + '=' + tableNumber * i +"<br> <br>")
+  }
+
+
+
+
