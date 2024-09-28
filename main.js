662c78f5b318aeb5ce034160dce212e8ad26871b
zubairkhan891 <zubairkhan891891@gmail.com>
2024-09-28 16:49:00 +0500

    add files

---
+++ main.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "zub@google.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+ 
+ //accountId = 2
+
+ accountEmail =  "zub@google.com"
+ accountPassword = "123456"
+ accountCity = "Jaipur"
+ console.log(accountId);
+
+ console.table([accountId, accountEmail, accountPassword, accountCity])
+ 
